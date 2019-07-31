51462c5a8e16566e5ff709bcdcadfdce66b5dce1
Daniel Huckins <dhuckins1@bloomberg.net>
2019-07-31 15:11:20 +0000

    allow for custom ssl cert paths

--- upwork/ca_certs_locater.py
+++ upwork/ca_certs_locater.py
@@ -5,6 +5,8 @@ LINUX_PATH = '/etc/ssl/certs/ca-certificates.crt'
 # openssl installed using `brew install openssl`
 OSX_PATH = '/usr/local/etc/openssl/cert.pem'
 
+CUSTOM_SSL_CERT_PATH = os.getenv('UPWORK_SSL_CERT', '')
+
 
 def get():
     """Return a path to a certificate authority file.
@@ -17,6 +19,9 @@ def get():
 
     if os.path.exists(OSX_PATH):
         return OSX_PATH
+
+    if CUSTOM_SSL_CERT_PATH and os.path.exists(CUSTOM_SSL_CERT_PATH):
+        return CUSTOM_SSL_CERT_PATH
 
     # Fall back to the httplib2 default behavior by raising an
     # ImportError if we have not found the file.
